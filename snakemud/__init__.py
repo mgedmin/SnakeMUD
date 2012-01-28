09e159a8abff84c4ae04c84650ee453778e43464
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2012-01-28 13:59:11 +0200

    Added session.

--- snakemud/__init__.py
+++ snakemud/__init__.py
@@ -1,9 +1,12 @@
 from pyramid.config import Configurator
+from pyramid_beaker import session_factory_from_settings
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    session_factory = session_factory_from_settings(settings)
+    config = Configurator(settings=settings,
+                          session_factory=session_factory)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
